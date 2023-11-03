15e5379daeaa62b0c4102ecdb7f5b6a9b9a4f520
thorikap1440 <torithaja@gmail.com>
2023-11-03 20:03:07 +0700

    perbaikan responsive csss dan font

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,8 @@ const navbar = document.getElementsByTagName("nav")[0];
 window.addEventListener("scroll", function () {
   console.log(window.scrollY);
   if (window.scrollY > 1) {
-    navbar.classList.replace("bg-transparent", "nav-color");
+    navbar.classList.add("nav-color", "shadow-sm");
   } else if (this.window.scrollY <= 0) {
-    navbar.classList.replace("nav-color", "bg-transparent");
+    navbar.classList.replace("shadow-sm", "nav-color");
   }
 });
