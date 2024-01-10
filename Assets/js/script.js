a2a41466d458a8697e299bf2b077ee0062583945
thorikap1440 <torithaja@gmail.com>
2024-01-10 17:48:15 +0700

    add file detail produk

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -168,3 +168,21 @@ document.addEventListener("DOMContentLoaded", function () {
   prevPageBtn.addEventListener("click", showPreviousPage);
   nextPageBtn.addEventListener("click", showNextPage);
 });
+
+// FUNGSI DROPDOWN UNTUK AVATAR
+function toggleDropdown() {
+  var dropdown = document.getElementById("avatarDropdown");
+  dropdown.classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".avatar")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
